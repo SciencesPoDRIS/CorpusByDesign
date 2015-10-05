a707a6b9de0d947e3a2758f33dcdbeae50e6c4ba
Anne Lhote <anne.lhote@gmail.com>
2015-10-05 12:42:35 +0200

    Adjust the filters facets height when not collapsed

--- app/js/app.js
+++ app/js/app.js
@@ -8,9 +8,15 @@
     app.controller('CorpusSnippetCtrl', ['$scope', 
         function($scope) {
             $scope.isCollapsed = true;
-            // $scope.collapsedFilters = true;
             $scope.collapseFilters = function() {
                 $scope.isCollapsed = !$scope.isCollapsed;
+                if(!$scope.isCollapsed) {
+                    $('.content .filters').height('100%');
+                    $('.content .filters .facets').height('100%');
+                } else {
+                    $('.content .filters').height('150px');
+                    $('.content .filters .facets').height('initial');
+                }
             }
         }
     ]);
