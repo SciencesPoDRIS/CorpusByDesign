176eb946bdc747935d021f223c95e7d687806bca
Anne Lhote <anne.lhote@gmail.com>
2015-10-07 14:59:41 +0200

    Scroll filters when uncollapsed

--- app/js/app.js
+++ app/js/app.js
@@ -12,10 +12,8 @@
                 $scope.isCollapsed = !$scope.isCollapsed;
                 if(!$scope.isCollapsed) {
                     $('.content .filters').height('100%');
-                    $('.content .filters .facets').height('100%');
                 } else {
                     $('.content .filters').height('150px');
-                    $('.content .filters .facets').height('initial');
                 }
             }
         }
