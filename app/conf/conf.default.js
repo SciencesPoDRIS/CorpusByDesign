31cc9dcdf43fa5ffac30f9c5e4e2fad9e841113b
Anne Lhote <anne.lhote@gmail.com>
2015-10-15 10:58:15 +0200

    Restore config files

---
+++ app/conf/conf.default.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    // The configuration is written as an Angular service
+    angular.module('webcorpus.conf', [])
+        // Google Analytics Id
+        .constant('googleAnalyticsId', 'XX-XXXXXXXX-X');
+
+})();
