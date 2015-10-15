579183c01828ccf1a1369d17a27ee544cbfb0526
Anne Lhote <anne.lhote@gmail.com>
2015-10-15 11:45:07 +0200

    Clean JS imports and files

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,30 @@
 (function() {
     'use strict';
 
+    /* Load modules */
     var app = angular.module('webcorpus', [
-        'ui.bootstrap',
         'ngRoute',
+        'ngMaterial',
+        'ui.bootstrap',
+        'webcorpus.conf',
+        'webcorpus.controllers',
+        'webcorpus.directives',
+        'webcorpus.filters',
+        'webcorpus.services',
         'webcorpus.corpus'
     ]);
 
+    /* Routing configuration */
     app.config(['$routeProvider', function($routeProvider) {
       $routeProvider.otherwise({redirectTo: '/'});
     }]);
+
+    /* Google analytics configuration */
+    app.run(function(googleAnalyticsId, $rootScope, $location) {
+        $rootScope.$on('$routeChangeSuccess', function() {
+            ga('create', googleAnalyticsId, 'auto');
+            ga('send', 'pageview', {'page': $location.path()});
+        });
+    });
 
 })();
