683884341159342730c695545b6d898ae2444fd4
Anne Lhote <anne.lhote@gmail.com>
2015-10-21 10:27:31 +0200

    Use the factory to load the corpus

--- app/views/corpus/corpus.js
+++ app/views/corpus/corpus.js
@@ -2,31 +2,37 @@
 'use strict';
 
     var app = angular.module('webcorpus.corpus', [
-        'ngRoute'
+        'ngRoute',
+        'webcorpus.services'
     ]);
 
-    app.config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/', {
-            templateUrl: 'views/corpus/corpus.html',
-            controller: 'CorpusController'
-        })
-    }]);
+    app.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.when('/', {
+                templateUrl: 'views/corpus/corpus.html',
+                controller: 'CorpusController'
+            })
+        }
+    ]);
 
-    app.controller('CorpusController', function($scope) {
-        /*
-        loadCorpora.getCorpora().then(function(data) {
-            $scope.corpus = data[0];
-        });
-        */
-        $scope.isCollapsed = true;
-        $scope.collapseFilters = function() {
-            $scope.isCollapsed = !$scope.isCollapsed;
-            if(!$scope.isCollapsed) {
-                $('.content .filters').height('100%');
-            } else {
-                $('.content .filters').height('150px');
+    app.controller('CorpusController', ['$scope', 'loadCorpora',
+        function($scope, loadCorpora) {
+            // Load the corpus
+            loadCorpora.getCorpora().then(function(data) {
+                $scope.corpus = data[0];
+            });
+            $scope.isCollapsed = true;
+            $scope.collapseFilters = function() {
+                $scope.isCollapsed = !$scope.isCollapsed;
+                if(!$scope.isCollapsed) {
+                    $('.content .filters').height('100%');
+                } else {
+                    $('.content .filters').height('150px');
+                }
             }
         }
-    });
+    ]);
+
+    
 
 })();
