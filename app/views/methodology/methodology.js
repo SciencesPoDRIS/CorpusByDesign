b4b5c1d4f4c5591bca9a380f8f5e5214a7bc9edb
Anne Lhote <anne.lhote@gmail.com>
2016-07-01 15:39:04 +0200

    Filter as HTML with the new filter

--- app/views/methodology/methodology.js
+++ app/views/methodology/methodology.js
@@ -12,15 +12,6 @@
             // Load all the corpora descriptions
             loadCorpus.getCorpus($scope.corpusId).then(function(corpus) {
                 $scope.corpus = corpus;
-                $scope.purpose = $sce.trustAsHtml($scope.corpus.purpose);
-                $scope.selection = $sce.trustAsHtml($scope.corpus.selection);
-                $scope.indexing = $sce.trustAsHtml($scope.corpus.indexing);
-                $scope.footnote = $sce.trustAsHtml($scope.corpus.footnote);
-                $scope.creation = $sce.trustAsHtml($scope.corpus.creation);
-                $scope.update = $sce.trustAsHtml($scope.corpus.update);
-                $scope.authors = $sce.trustAsHtml($scope.corpus.authors);
-                $scope.size = $sce.trustAsHtml($scope.corpus.size);
-                $scope.subtitle = $sce.trustAsHtml($scope.corpus.subtitle);
             });
         }
     ]);
